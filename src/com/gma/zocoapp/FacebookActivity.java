832cce6cdefe7c2ad05984c830339d9d25c6805b
Raul Ascencio <raul@Greens-iMac.local>
2013-12-16 16:35:59 -0600

    Social Login Enabled
    TODO:
        * Fetch user data for both social networks
        * Make ZocoApi a Singleton or something similiar.

--- src/com/gma/zocoapp/FacebookActivity.java
+++ src/com/gma/zocoapp/FacebookActivity.java
@@ -2,21 +2,62 @@ package com.gma.zocoapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
+import com.facebook.*;
+import com.facebook.model.*;
 
 public class FacebookActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_facebook);
+		setContentView(R.layout.activity_main);
+
+		// start Facebook Login
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+			// callback when session changes state
+			@Override
+			public void call(Session session, SessionState state,
+					Exception exception) {
+				if (session.isOpened()) {
+
+					// make request to the /me API
+					Request.executeMeRequestAsync(session,
+							new Request.GraphUserCallback() {
+
+								TextView welcome = (TextView) findViewById(R.id.sign_in_status);
+
+								// callback after Graph API response with user
+								// object
+								@Override
+								public void onCompleted(GraphUser user,
+										Response response) {
+									if (user != null) {
+										welcome.setText("Hello "
+												+ user.getName() + "!");
+										Log.d(FacebookActivity.class
+												.getCanonicalName(), user
+												.getName());
+									} else {
+										welcome.setText("Foo is bar as bar is foo");
+										Log.d(FacebookActivity.class
+												.getCanonicalName(), "Fuck U");
+									}
+								}
+							});
+				}
+			}
+		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.facebook, menu);
-		return true;
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode,
+				resultCode, data);
 	}
 
 }
--- src/com/gma/zocoapp/utils/Res.java
+++ src/com/gma/zocoapp/utils/Res.java
@@ -30,4 +30,24 @@ public final class Res {
 	public static String getUrlUsers() {
 		return urlBase + "users";
 	}
+
+	// public void printHashKey() {
+	//
+	// try {
+	// PackageInfo info = getPackageManager().getPackageInfo(
+	// "com.gma.zocoapp", PackageManager.GET_SIGNATURES);
+	// for (Signature signature : info.signatures) {
+	// MessageDigest md = MessageDigest.getInstance("SHA");
+	// md.update(signature.toByteArray());
+	// Log.d("TEMPTAGHASH KEY:",
+	// Base64.encodeToString(md.digest(), Base64.DEFAULT));
+	// }
+	// } catch (NameNotFoundException e) {
+	//
+	// } catch (NoSuchAlgorithmException e) {
+	//
+	// }
+	//
+	// }
+
 }
